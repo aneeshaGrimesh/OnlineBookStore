e275372638578b58f6dddbdf42e8f2ca513288e3
Aneesha <aneesha.nu123@gmail.com>
2023-11-15 16:25:11 +0530

    online book store asp .net project

---
+++ OnlineBookShopTuto/Views/Login.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="OnlineBookShopTuto.Views.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="../Assets/Lib/css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row mt-5 mb-5">
+
+        </div>
+         <div class="row">
+             <div class="col-md-4">
+
+             </div>
+               <div class="col-md-4">
+             <form id="form1" runat="server">
+                 <div>
+                     <div class="row">
+                         <h4 class="text-center text-success">Book Shop Management System</h4>
+                         <div class="col-md-4"></div>
+                          <div class="col-md-8">
+                                <img src="../Assets/Images/couple_bill_100px.png" style="height:150px; width:150px;" /><br />
+                              
+                          </div>
+                     </div>
+                   
+                 </div>
+                  <div class="mt-3">
+                        <label for="" class="form-label">User Name</label>
+                        <input type="email" placeholder="Your Email Here" autocomplete="off" runat="server" class="form-control" id="UnameTb"/>
+                  </div>
+                  <div class="mt-3">
+                        <label for="" class="form-label">Password</label>
+                        <input type="password" placeholder="Password" autocomplete="off" runat="server" class="form-control" id="PasswordTb"/>
+                  </div>
+                 <div class="mt-3 d-grid">
+                      <asp:Label runat="server" ID="ErrMsg" class="text-danger text-center"></asp:Label><br />
+                        <asp:Button Text="Login" runat="server" class="btn-success btn" ID="LoginBtn" OnClick="LoginBtn_Click"/>
+                  </div>
+             </form>
+             </div>
+              <div class="col-md-4">
+
+             </div>
+        </div>
+    </div>
+   
+</body>
+</html>
---
+++ OnlineBookShopTuto/Views/Seller/Selling.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineBookShopTuto.Views.Seller
+{
+    public partial class Selling : System.Web.UI.Page
+    {
+        Models.Functions Con;
+        int Seller = Login.User;
+        string SName = Login.UName;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Con = new Models.Functions();
+            if (!IsPostBack)
+            {
+                ShowBooks();
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[5]
+                {
+                    new DataColumn("ID"),
+                    new DataColumn("Book"),
+                    new DataColumn("Price"),
+                    new DataColumn("Quantity"),
+                    new DataColumn("Total")
+                }
+                );
+                ViewState["Bill"] = dt;
+                this.BindGrid();
+            }
+        }
+        protected void BindGrid()
+        {
+            BillList.DataSource = ViewState["Bill"];
+            BillList.DataBind();
+        }
+        private void ShowBooks()
+        {
+            string Query = "Select BId as Code,BName as Name,BQty as Stock,Bprice as Price from BookTbl";
+            BooksList.DataSource = Con.GetData(Query);
+            BooksList.DataBind();
+
+        }
+        int Key = 0;
+        int Stock = 0;
+        protected void AuthorsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BNameTb.Value = BooksList.SelectedRow.Cells[2].Text;
+            Stock= Convert.ToInt32(BooksList.SelectedRow.Cells[3].Text);
+            BPriceTb.Value = BooksList.SelectedRow.Cells[4].Text;
+          
+            if (BNameTb.Value == "")
+            {
+                Key = 0;
+            }
+            else
+            {
+                Key = Convert.ToInt32(BooksList.SelectedRow.Cells[1].Text);
+            }
+        }
+        private void UpdateStock()
+        {
+            int NewQty;
+            NewQty = Convert.ToInt32(BooksList.SelectedRow.Cells[3].Text) - Convert.ToInt32(BQtyTb.Value);
+     
+            string Query = "update BookTbl set BQty = {0} where BId = {1}";
+            Query = string.Format(Query, NewQty, BooksList.SelectedRow.Cells[1].Text);
+            Con.SetData(Query);
+            ShowBooks();
+
+        }
+        private void InsertBill()
+        {
+            string Query = "insert into BillTbl values('{0}',{1},{2})";
+            Query = string.Format(Query, System.DateTime.Today.Date.ToString(), Seller, Convert.ToInt32(GrdTotalTb.Text.Substring(2)));
+            Con.SetData(Query);
+            
+        }
+        int Grdtotal = 0;
+        int Amount;
+        
+
+        protected void AddToBillBtn_Click(object sender, EventArgs e)
+        {
+            if(BQtyTb.Value == "" || BPriceTb.Value == "" || BNameTb.Value == "")
+            {
+
+            }else
+            {
+                int total = Convert.ToInt32(BQtyTb.Value) * Convert.ToInt32(BPriceTb.Value);
+                DataTable dt = (DataTable)ViewState["Bill"];
+                dt.Rows.Add(BillList.Rows.Count + 1,
+                    BNameTb.Value.Trim(),
+                    BPriceTb.Value.Trim(),
+                    BQtyTb.Value.Trim(),
+                    total);
+
+                ViewState["Bill"] = dt;
+                this.BindGrid();
+                UpdateStock();
+               // Grdtotal = 0;
+                for (int i = 0; i < BillList.Rows.Count - 1; i++)
+                {
+                    Grdtotal = Grdtotal + Convert.ToInt32(BillList.Rows[i].Cells[5].Text);
+                }
+                Amount = Grdtotal;
+                GrdTotalTb.Text = "Rs" + Grdtotal;
+                BNameTb.Value = "";
+                BPriceTb.Value = "";
+                BQtyTb.Value = "";
+               
+            }
+            
+        }
+
+        protected void PrintBtn_Click(object sender, EventArgs e)
+        {
+            InsertBill();
+        }
+    }
+}
